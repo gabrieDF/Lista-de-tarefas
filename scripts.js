13398322fa0f623c81be89549ab559ee87db361a
gabrieDF <gabriel.d.feitosa@hotmail.com>
2022-01-19 21:30:58 -0300

    lista de tarefas

---
+++ scripts.js
@@ -0,0 +1,153 @@
+/*adicionar item com o btao enter do teclado */
+document.addEventListener("keypress", (enter) => {
+  if(enter.key === 'Enter') {
+      const btn = document.querySelector("#buttonEnter");
+
+    btn.click();
+  }
+});
+
+/*Selecionando input e button*/
+const inputElement = document.querySelector(".new-task-input");
+
+const addTaskButton = document.querySelector(".new-task-button");
+
+const tasksContainer = document.querySelector(".tasks-container");
+
+/*validando o campo do input para nao entrar strings vazias */
+const validateInput = () => inputElement.value.trim().length > 0;
+
+/*caso o input esteja vazio, exibir um evento de erro */
+const handleAddTask = () => {
+  const inputIsValid = validateInput();
+
+  console.log(inputIsValid);
+
+  if (!inputIsValid) {
+    return inputElement.classList.add("error");
+  }
+
+  /*Estrutura a ser criada para o itens armazenados */
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("task-item");
+
+  const taskContent = document.createElement("p");
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent));
+
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+
+  deleteItem.addEventListener("click", () =>
+    handleDeleteClick(taskItemContainer, taskContent)
+  );
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
+
+  inputElement.value = "";
+
+  updateLocalStorage();
+};
+
+/*style task */
+
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+    if (currentTaskIsBeingClicked) {
+      task.firstChild.classList.toggle("completed");
+    }
+  }
+
+  updateLocalStorage();
+};
+
+
+/*Delete item/task*/
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+    if (currentTaskIsBeingClicked) {
+      taskItemContainer.remove();
+    }
+  }
+
+  updateLocalStorage();
+};
+
+
+/*funcao para tirar o error do input */
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if (inputIsValid) {
+    return inputElement.classList.remove("error");
+  }
+};
+
+
+/*Local storage */
+const updateLocalStorage = () => {
+  const tasks = tasksContainer.childNodes;
+
+  const localStorageTasks = [...tasks].map((task) => {
+    const content = task.firstChild;
+    const isCompleted = content.classList.contains("completed");
+
+    return { description: content.innerText, isCompleted };
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
+};
+
+const refreshTasksUsingLocalStorage = () => {
+  const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+
+  for (const task of tasksFromLocalStorage) {
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = task.description;
+
+    if (task.isCompleted) {
+      taskContent.classList.add("completed");
+    }
+
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener("click", () =>
+      handleDeleteClick(taskItemContainer, taskContent)
+    );
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+  }
+};
+
+/* chamando funcao refresh */
+refreshTasksUsingLocalStorage();
+
+/* funcao que add item */
+addTaskButton.addEventListener("click", () => handleAddTask());
+
+/* funcao que verifica o input */
+inputElement.addEventListener("change", () => handleInputChange());
